6e3827e159cfee8201e0771155df89acf52aa4b2
fahad <fahadukr@gmail.com>
2024-07-20 08:56:35 -0400

    update to v 0.4.3

--- utils_b_infra/caching/__init__.py
+++ utils_b_infra/caching/__init__.py
@@ -1,3 +1,4 @@
+from utils_b_infra.caching.backends.base import BaseCache
 from utils_b_infra.caching.config import CacheConfig
 from utils_b_infra.caching.utils import key_builder, run_coro_in_background
 
