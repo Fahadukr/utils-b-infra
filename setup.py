1427b0efe2ebd481fa47389ea924707010e43cea
fahad <fahadukr@gmail.com>
2025-10-07 18:03:42 -0400

    Bump version to 1.3.25 in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('CHANGELOG.md', 'r') as ch:
 
 setup(
     name="utils_b_infra",
-    version="1.3.24",
+    version="1.3.25",
     author="Fahad Mawlood",
     author_email="fahadukr@gmail.com",
     description="A collection of utility functions and classes for Python projects.",
